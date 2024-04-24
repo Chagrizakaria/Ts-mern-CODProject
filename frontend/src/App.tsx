c072be20c7f939712d74924db0019293580cc5c1
Chagrizakaria <zikochagr@gmail.com>
2024-04-24 23:35:15 +0100

    Video-07-Add-Page-Routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,32 @@
-import { sampleProducts } from './data'
-import {  Navbar,Container,Nav, Row, Col} from "react-bootstrap"
+import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
 function App() {
   return (
     <div className="d-flex flex-column vh-100">
-      <header>  
+      <header>
         <Navbar bg="dark" variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand>
-            CODProject
-          </Navbar.Brand>
-        </Container>
-        <Nav>
-<a href="/Cart" className='nav-link'>Cart</a>
-<a href="/Signin" className='nav-link'>Sign in</a>
-        </Nav>
-        </Navbar> </header>
+          <Container>
+            <Navbar.Brand>CODProject</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/Cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/Signin" className="nav-link">
+              Sign in
+            </a>
+          </Nav>
+        </Navbar>{' '}
+      </header>
       <main>
-        <Container className='mt-3'>
-        <Row>
-          {sampleProducts.map((product) => (
-            <Col key={product.slug} sm={6} md={4} lg={3}>
-              <img
-                src={product.image}
-                alt={product.name}
-                className="product-image"
-              />
-              <h2>{product.name}</h2>
-              <p>{product.price} DH</p>
-            </Col>
-          ))}
-        </Row>
+        <Container className="mt-3">
+          <Outlet />
         </Container>
-        
       </main>
-      <footer> <div className='text-center'> All right reserved
-        </div></footer>
+      <footer>
+        {' '}
+        <div className="text-center"> All right reserved</div>
+      </footer>
     </div>
   )
 }
